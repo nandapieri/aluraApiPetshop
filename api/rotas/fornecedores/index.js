bf3981551cf4e5659a213e1243775c2bae92531b
Fernanda Pieri <nandapieri@gmail.com>
2021-05-21 15:47:20 -0300

    adicionados status de resposta para todas as requisicoes

--- api/rotas/fornecedores/index.js
+++ api/rotas/fornecedores/index.js
@@ -4,6 +4,7 @@ const Fornecedor = require('./Fornecedor')
 
 roteador.get('/', async (requisicao, resposta) => {
     const resultados = await TabelaFornecedor.listar()
+    resposta.status(200)
     resposta.send(
         JSON.stringify(resultados)
     )
@@ -14,11 +15,13 @@ roteador.post('/', async (requisicao, resposta) => {
     const dadosRecebidos = requisicao.body;
     const fornecedor = new Fornecedor(dadosRecebidos);
     await fornecedor.criar();
+    resposta.status(201)
     resposta.send(
       JSON.stringify(fornecedor)
     )
   }
   catch (erro) {
+    resposta.status(400)
     resposta.send(
         JSON.stringify({
           mensagem: erro.message
@@ -32,11 +35,13 @@ roteador.get('/:idFornecedor', async (requisicao, resposta) => {
       const id = requisicao.params.idFornecedor
       const fornecedor = new Fornecedor({ id: id });
       await fornecedor.carregar()
+      resposta.status(200)
       resposta.send(
           JSON.stringify(fornecedor)
       )
     }
     catch (erro) {
+      resposta.status(404)
       resposta.send(
           JSON.stringify({
             mensagem: erro.message
@@ -52,9 +57,11 @@ roteador.put('/:idFornecedor', async (requisicao, resposta) => {
     const dados = Object.assign({},dadosRecebidos, { id: id })
     const fornecedor = new Fornecedor(dados);
     await fornecedor.atualizar()
+    resposta.status(204)
     resposta.end()
   }
   catch (erro) {
+    resposta.status(400)
     resposta.send(
         JSON.stringify({
           mensagem: erro.message
@@ -69,9 +76,11 @@ roteador.delete('/:idFornecedor', async (requisicao, resposta) => {
     const fornecedor = new Fornecedor({ id: id });
     await fornecedor.carregar()
     await fornecedor.remover()
+    resposta.status(204)
     resposta.end()
   }
   catch (erro) {
+    resposta.status(404)
     resposta.send(
         JSON.stringify({
           mensagem: erro.message
