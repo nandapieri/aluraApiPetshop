49b4bd661a730bc50e2da7370ec8a170a8a004f0
Fernanda Pieri <nandapieri@gmail.com>
2021-05-25 16:51:11 -0300

    adicionada filtragem dos dados para não exibir dados sensíveis para o cliente no serializador

--- api/Serializador.js
+++ api/Serializador.js
@@ -1,27 +1,59 @@
 const ValorNaoSuportado = require('./erros/ValorNaoSuportado')
 
 class Serializador {
-  json (dados) {
-    return JSON.stringify(dados)
-  }
-
-  serializar(dados) {
-    if (this.contentType === 'application/json') {
-      return this.json(dados)
+    json (dados) {
+        return JSON.stringify(dados)
     }
-    throw new ValorNaoSuportado(this.contentType)
-  }
+
+    serializar (dados) {
+        if (this.contentType === 'application/json') {
+            return this.json(
+                this.filtrar(dados)
+            )
+        }
+
+        throw new ValorNaoSuportado(this.contentType)
+    }
+
+    filtrarObjeto (dados) {
+        const novoObjeto = {}
+
+        this.camposPublicos.forEach((campo) => {
+            if (dados.hasOwnProperty(campo)) {
+                novoObjeto[campo] = dados[campo]
+            }
+        })
+
+        return novoObjeto
+    }
+
+    filtrar (dados) {
+        if (Array.isArray(dados)) {
+            dados = dados.map(item => {
+                return this.filtrarObjeto(item)
+            })
+        } else {
+            dados = this.filtrarObjeto(dados)
+        }
+
+        return dados
+    }
 }
 
 class SerializadorFornecedor extends Serializador {
-  constructor(contentType) {
-    super()
-    this.contentType = contentType
-  }
+    constructor (contentType) {
+        super()
+        this.contentType = contentType
+        this.camposPublicos = [
+            'id',
+            'empresa',
+            'categoria'
+        ]
+    }
 }
 
 module.exports = {
-  Serializador: Serializador,
-  SerializadorFornecedor: SerializadorFornecedor,
-  formatosAceitos: ['application/json', '*/*']
+    Serializador: Serializador,
+    SerializadorFornecedor: SerializadorFornecedor,
+    formatosAceitos: ['application/json']
 }
