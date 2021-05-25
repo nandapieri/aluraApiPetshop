1ec60e3161d6e0eb514452ac62423857ed0a2744
Fernanda Pieri <nandapieri@gmail.com>
2021-05-25 17:26:53 -0300

    adicionado suporte a requisições em xml

--- api/Serializador.js
+++ api/Serializador.js
@@ -1,15 +1,30 @@
 const ValorNaoSuportado = require('./erros/ValorNaoSuportado')
+const jsontoxml = require('jsontoxml')
 
 class Serializador {
     json (dados) {
         return JSON.stringify(dados)
     }
 
+    xml (dados) {
+        let tag = this.tagSingular
+        if (Array.isArray(dados)) {
+          tag = this.tagPlural
+          dados = dados.map((item) => {
+            return { [this.tagSinfular]:item }
+          })
+        }
+        return jsontoxml({ [tag]: dados })
+    }
+
     serializar (dados) {
+        dados = this.filtrar(dados);
         if (this.contentType === 'application/json') {
-            return this.json(
-                this.filtrar(dados)
-            )
+            return this.json(dados)
+        }
+
+        if (this.contentType === 'application/xml') {
+            return this.xml(dados)
         }
 
         throw new ValorNaoSuportado(this.contentType)
@@ -49,11 +64,27 @@ class SerializadorFornecedor extends Serializador {
             'empresa',
             'categoria'
         ].concat(camposExtras || [])
+        this.tagSinfular = 'fornecedor'
+        this.tagPlural = 'fornecedores'
     }
+}
+
+class SerializadorErro extends Serializador {
+  constructor(contentType, camposExtras) {
+    super()
+    this.contentType = contentType
+    this.camposPublicos = [
+        'id',
+        'mensagem'
+    ].concat(camposExtras || [])
+    this.tagSinfular = 'erro'
+    this.tagPlural = 'erros'
+  }
 }
 
 module.exports = {
     Serializador: Serializador,
     SerializadorFornecedor: SerializadorFornecedor,
-    formatosAceitos: ['application/json']
+    SerializadorErro:SerializadorErro,
+    formatosAceitos: ['application/json','application/xml']
 }
